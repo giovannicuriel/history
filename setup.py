b34f248def0d0c8046992a45e1c75d6e75d49929
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2019-01-05 11:47:28 -0200

    chore: updating setup.py script

--- setup.py
+++ setup.py
@@ -1,53 +1,88 @@
 # -*- coding: utf-8 -*-
+"""
+setup.py script
+"""
+
+import io
+from collections import OrderedDict
+from setuptools import setup
 import os
 
-import setuptools
-
-try:
-    from pip import download
-    from pip import req
-except:
-    from pip._internal import download
-    from pip._internal import req
-
-HERE = os.path.abspath(os.path.dirname(__file__))
+with io.open("README.md", "rt", encoding="utf8") as f:
+    README = f.read()
 
 
-def get_requirements(reqfile):
-    path = os.path.join(HERE, reqfile)
-    deps = list()
-    
-    for dep in req.parse_requirements(path, session=download.PipSession()):
-        try:
-            # Pip 8.1.2 Compatible
-            specs = ','.join(''.join(str(spec)) for spec in dep.req.specifier)
-        except AttributeError:
-            # Pip 1.5.4 Compatible
-            specs = ','.join(''.join(spec) for spec in dep.req.specs)
-        requirement = '{name}{extras}{specs}'.format(
-            name=dep.name,
-            extras=(
-                '[{extras}]'.format(extras=','.join(dep.extras))
-                if dep.extras else ''
-            ),
-            specs=specs,
-        )
-
-        deps.append(requirement)
-    return deps
-
-
-setuptools.setup(
-    name='history',
-    description='History retriever and manager',
-    version=':versiontools:history:',
-
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    install_requires=get_requirements('requirements/requirements.txt'),
-    setup_requires=('versiontools'),
-
-    author='Matheus Magalhaes',
-    author_email='matheusr@cpqd.com.br',
-    url='dojot.com.br',
-)
+if os.environ.get("HISTORY_MODULE", None) == "history":
+    setup(
+        name="dojot.history",
+        version="1.0.0",
+        url="http://github.com/dojot/history",
+        project_urls=OrderedDict((
+            ("Code", "https://github.com/dojot/history.git"),
+            ("Issue tracker", "https://github.com/dojot/history/issues"),
+        )),
+        license="GPL-3.0",
+        author="Giovanni Curiel dos Santos",
+        author_email="giovannicuriel@gmail.com",
+        maintainer="dojot team",
+        description="History retriever and manager",
+        long_description=README,
+        packages=["dojot.history"],
+        include_package_data=True,
+        zip_safe=False,
+        platforms=[any],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=[
+            "falcon==1.0.0",
+            "gunicorn==19.6.0",
+            "gevent==1.1.1",
+            "pymongo==3.2.2",
+            "python-dateutil==2.7.5",
+            "requests==2.20.0",
+        ],
+        extras_require={
+            "dev": [
+                "pytest==4.0.0",
+                "pytest-cov==2.6.0"
+            ]
+        }
+    )
+elif os.environ.get("HISTORY_MODULE", None) == "persister":
+    setup(
+        name="dojot.persister",
+        version="1.0.0",
+        url="http://github.com/dojot/history",
+        project_urls=OrderedDict((
+            ("Code", "https://github.com/dojot/history.git"),
+            ("Issue tracker", "https://github.com/dojot/history/issues"),
+        )),
+        license="GPL-3.0",
+        author="Giovanni Curiel dos Santos",
+        author_email="giovannicuriel@gmail.com",
+        maintainer="dojot team",
+        description="History retriever and manager",
+        long_description=README,
+        packages=["dojot.persister"],
+        include_package_data=True,
+        zip_safe=False,
+        platforms=[any],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=[
+            "pymongo==3.2.2",
+            "dojot.module==0.0.1a1"
+        ],
+        extras_require={
+            "dev": [
+                "pytest==4.0.0",
+                "pytest-cov==2.6.0"
+            ]
+        }
+    )
